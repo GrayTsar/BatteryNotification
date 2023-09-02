49428055b6056e4e86d554674eb40b0093f081f8
Viatcheslav Ehrmann <viatcheslav.ehrmann@gmail.com>
2023-09-02 15:08:20 +0200

    #release v1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.graytsar.batterynotification"
         minSdk = 23
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.5"
+        versionCode = 8
+        versionName = "1.0.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables.useSupportLibrary = true
